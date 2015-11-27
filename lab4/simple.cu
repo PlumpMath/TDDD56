32549ca6f4a8ad38678ecf2941066763238319e0
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-11-27 11:27:36 +0100

    Completed lab 4.1.

--- lab4/simple.cu
+++ lab4/simple.cu
@@ -5,31 +5,33 @@
 
 #include <stdio.h>
 
-const int N = 16; 
-const int blocksize = 16; 
+const int N = 16;
+const int blocksize = 16;
 
-__global__ 
-void simple(float *c) 
+__global__
+void simple(float *c)
 {
-	c[threadIdx.x] = threadIdx.x;
+	c[threadIdx.x] = sqrt((float)threadIdx.x);
 }
 
 int main()
 {
-	float *c = new float[N];	
+	float *c = new float[N];
 	float *cd;
 	const int size = N*sizeof(float);
-	
+
 	cudaMalloc( (void**)&cd, size );
 	dim3 dimBlock( blocksize, 1 );
 	dim3 dimGrid( 1, 1 );
 	simple<<<dimGrid, dimBlock>>>(cd);
 	cudaThreadSynchronize();
-	cudaMemcpy( c, cd, size, cudaMemcpyDeviceToHost ); 
+	cudaMemcpy( c, cd, size, cudaMemcpyDeviceToHost );
 	cudaFree( cd );
-	
-	for (int i = 0; i < N; i++)
-		printf("%f ", c[i]);
+
+	for (int i = 0; i < N; i++){
+		printf("%.17g ", c[i]);
+		printf("%.17g\n", sqrt((double)i));
+	}
 	printf("\n");
 	delete[] c;
 	printf("done\n");
