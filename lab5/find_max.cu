f1ba08b4f3b1afe688bf85a97c94e2e8d89fbaf1
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-12-13 14:33:35 +0100

    It sort of works, but it's dog slow and only sort of works.

--- lab5/find_max.cu
+++ lab5/find_max.cu
@@ -7,42 +7,49 @@
 #define SIZE 1024
 
 
-__global__ void find_max(int *data, int N) {
-  int index = threadIdx.x + blockDim.x * blockIdx.x;
-
-	int m = data[index];
-	for (int i = 0; i < SIZE / blockDim.x / gridDim.x; i++)
+__global__ void find_max(unsigned int *data, unsigned int intsPerThread) {
+  unsigned int index = threadIdx.x + blockDim.x * blockIdx.x;
+	unsigned int m = data[index];
+	for (unsigned int i = 0; i < intsPerThread; i++)
 		if (data[index + i] > m)
 			m = data[index + i];
   data[index] = m;
 
-	if (threadIdx.x == 0) {
-		for (int i = 0; i < SIZE / blockDim.x / gridDim.x; i++)
-			if (data[index + i * blockDim.x] > m)
-				m = data[index + i * blockDim.x];
-		data[index] = m;
+	__syncthreads();
 
-		if (blockIdx.x == 0) {
-			for (int i = 0; i < SIZE / blockDim.x / gridDim.x; i++)
-				if (data[i * blockDim.x * gridDim.x] > m)
-					m = data[i * blockDim.x * gridDim.x];
-			data[index] = m;
-		}
+	if (threadIdx.x == 0) {
+		for (unsigned int i = 0; i < blockDim.x; i++)
+			if (data[index + i * intsPerThread] > m)
+				m = data[index + i * intsPerThread];
+		data[index] = m;
 	}
 }
 
-void launch_cuda_kernel(int *data, int N) {
+__global__ void find_max_between_blocks(unsigned int *data, unsigned int intsPerThread, unsigned int gridSize, unsigned int blockSize) {
+	unsigned int m = data[0];
+	for (unsigned int i = 0; i < gridSize; i++)
+		if (data[i * blockSize * intsPerThread] > m)
+			m = data[i * blockSize * intsPerThread];
+	data[0] = m;
+
+}
+
+void launch_cuda_kernel(unsigned int *data, unsigned int N) {
 	// Handle your CUDA kernel launches in this function
 
-	int *devdata;
-	int size = sizeof(int) * N;
+	unsigned int *devdata;
+	unsigned int size = sizeof(int) * N;
 	cudaMalloc( (void**)&devdata, size);
 	cudaMemcpy(devdata, data, size, cudaMemcpyHostToDevice );
 
 	// Dummy launch
-	dim3 dimBlock(8, 1);
+	dim3 dimBlock(16, 1);
 	dim3 dimGrid(8, 1);
-	find_max<<<dimGrid, dimBlock>>>(devdata, N);
+	dim3 oneGrid(1, 1);
+	unsigned int intsPerThread = SIZE / 8 / 16;
+	find_max<<<dimGrid, dimBlock>>>(devdata, intsPerThread);
+	cudaThreadSynchronize();
+	find_max_between_blocks<<<oneGrid, oneGrid>>>(devdata, intsPerThread, dimGrid.x, dimBlock.x);
 	cudaError_t err = cudaPeekAtLastError();
 	if (err) printf("cudaPeekAtLastError %d %s\n", err, cudaGetErrorString(err));
 
@@ -52,8 +59,8 @@ void launch_cuda_kernel(int *data, int N) {
 }
 
 // CPU max finder (sequential)
-void find_max_cpu(int *data, int N) {
-  int i, m;
+void find_max_cpu(unsigned int *data, unsigned int N) {
+  unsigned int i, m;
 
 	m = data[0];
 	for (i = 0; i < N; i++) {
@@ -64,8 +71,8 @@ void find_max_cpu(int *data, int N) {
 }
 
 // Dummy data in comments below for testing
-int data[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
-int data2[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
+unsigned int data[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
+unsigned int data2[SIZE];// = {1, 2, 5, 3, 6, 8, 5, 3, 1, 65, 8, 5, 3, 34, 2, 54};
 
 int main() {
   // Generate 2 copies of random data
@@ -84,7 +91,7 @@ int main() {
   launch_cuda_kernel(data2, SIZE);
   printf("GPU time %f\n", GetSeconds());
 
-  // Print result
+  // Prunsigned int result
   printf("\n");
   printf("CPU found max %d\n", data[0]);
   printf("GPU found max %d\n", data2[0]);
